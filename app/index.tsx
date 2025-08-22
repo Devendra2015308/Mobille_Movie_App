07d4a936b22d152663d8ed038c1762b60f046fc6
Devendra Kumar <Devendra2015308@akgec.ac.in>
2025-08-22 09:17:56 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray">
+      <Text className="text-5xl font-bold text-accent">Hello Dev</Text>
+      <Link href="/onboarding">Onboarding</Link>
+    </View>
+  );
+}
