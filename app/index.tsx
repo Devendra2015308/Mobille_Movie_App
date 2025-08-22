9bca9a84a8925fc39c4192aed386640ae91b794e
Devendra Kumar <Devendra2015308@akgec.ac.in>
2025-08-22 09:19:38 +0530

    update

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-gray">
-      <Text className="text-5xl font-bold text-accent">Hello Dev</Text>
+      <Text className="text-5xl font-bold text-accent">Hello Devendra</Text>
       <Link href="/onboarding">Onboarding</Link>
     </View>
   );
